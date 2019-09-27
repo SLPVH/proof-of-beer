97d1b9611fe04b941943d28dc9f184fef6d0afb6
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2019-09-27 22:21:07 +0000

    mint example

---
+++ functions/mint.js
@@ -0,0 +1,53 @@
+
+const SLPSDK = require('slp-sdk/lib/SLP')
+let SLP = new SLPSDK({ restURL: 'https://rest.bitcoin.com/v2/' })
+
+function priv_to_publickey(privkey_wif) {
+    let ecPair = SLP.ECPair.fromWIF(privkey_wif)
+    let cashAddress = SLP.ECPair.toCashAddress(ecPair)
+    let slpAddress = SLP.ECPair.toSLPAddress(ecPair)
+    return [cashAddress, slpAddress]
+}
+
+// Mint quantity of tokenid to dst_addr.
+// * Minting baton needs to be in privkey.
+// * Privkey needs to hold BCH for fees.
+// * Privkey receives change
+async function mintbeer(tokenid, quantity, privkey_wif, dst_slpaddr) {
+    let [cashAddress, slpAddress] = priv_to_publickey(privkey_wif)
+    console.log(cashAddress)
+    console.log(slpAddress)
+
+    const mintConfig = {
+        fundingAddress: slpAddress,
+        fundingWif: privkey_wif,
+        tokenReceiverAddress: dst_slpaddr,
+        batonReceiverAddress: slpAddress,
+        bchChangeReceiverAddress: cashAddress,
+        tokenId: tokenid,
+        additionalTokenQty: quantity
+    }
+    console.log(mintConfig)
+
+    const mintTxId = await SLP.TokenType1.mint(mintConfig)
+    return mintTxId;
+}
+
+async function test(dst_slpaddr) {
+    let mnemonic = "stuff goes here"
+    let rootseed = SLP.Mnemonic.toSeed(mnemonic)
+    let hdnode = SLP.HDNode.fromSeed(rootseed)
+    const account = SLP.HDNode.derivePath(hdnode, "m/44'/145'/0'")
+    const change = SLP.HDNode.derivePath(account, "0/0")
+    const privkey = SLP.HDNode.toWIF(change)
+
+    try {
+        let tokenid = "364f1366f32a4d3b15945300964e824754fc5434a3ee892b7718a912a464f56a"
+        let txid = await mintbeer(tokenid, 100, privkey, dst_slpaddr)
+        console.log("Success! " + txid)
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
+// test("simpleledger:qqv9hfwy5jfmuz5gnyrqf7u6snqk4gk0jv829es2kz")
