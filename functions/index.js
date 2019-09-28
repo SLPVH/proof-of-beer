a0abae2a1697c14799adce7bc74ec83aedbc4a4a
Jørgen Svennevik Notland <jorgen.notland@protonmail.ch>
2019-09-28 21:12:00 +0200

    asd

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
 const create_event = require('./create.js').create;
+let slputil = require("./slputil")
 
 // We have to import the built version of the server middleware.
 const { sapper } = require('./__sapper__/build/server/server');
@@ -22,3 +24,27 @@ exports.create_token = functions.https.onCall(async (data, context) => {
     }) 
     res.send(txid)
 })
+
+exports.createUserData = functions.auth.user().onCreate((user) => {
+    let SLP = slputil.get_slpsdk()
+
+    const private_key = function gen_privkey() {
+        let mnemonic = SLP.Mnemonic.generate()
+        let rootseed = SLP.Mnemonic.toSeed(mnemonic)
+        let hdnode = SLP.HDNode.fromSeed(rootseed)
+        const account = SLP.HDNode.derivePath(hdnode, "m/44'/145'/0'")
+        const privkey = SLP.HDNode.toWIF(account)
+        return privkey
+    }
+    
+    const [cash_addr, slp_addr] = slputil.priv_to_publickey(SLP, private_key)
+
+    firebase.database().ref('users/' + userId).set({
+        private_key: private_key,
+        cash_addr: cash_addr,
+        slp_addr: slp_addr,
+        email: user.email
+      });
+
+  });
+
