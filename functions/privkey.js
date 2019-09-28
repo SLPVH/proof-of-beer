3a4bf765c8ee6eb615f120b71173b7e991e6b5bf
Jørgen Svennevik Notland <jorgen.notland@protonmail.ch>
2019-09-28 21:04:10 +0200

    save user data

--- functions/privkey.js
+++ functions/privkey.js
@@ -1,12 +1,25 @@
 let slputil = require("./slputil")
 
-function gen_privkey() {
+exports.createUserData = functions.auth.user().onCreate((user) => {
     let SLP = slputil.get_slpsdk()
-    let mnemonic = SLP.Mnemonic.generate()
-    let rootseed = SLP.Mnemonic.toSeed(mnemonic)
-    let hdnode = SLP.HDNode.fromSeed(rootseed)
-    const account = SLP.HDNode.derivePath(hdnode, "m/44'/145'/0'")
-    const privkey = SLP.HDNode.toWIF(account)
-    return privkey
-}
+
+    const private_key = function gen_privkey() {
+        let mnemonic = SLP.Mnemonic.generate()
+        let rootseed = SLP.Mnemonic.toSeed(mnemonic)
+        let hdnode = SLP.HDNode.fromSeed(rootseed)
+        const account = SLP.HDNode.derivePath(hdnode, "m/44'/145'/0'")
+        const privkey = SLP.HDNode.toWIF(account)
+        return privkey
+    }
+    
+    const [cash_addr, slp_addr] = slputil.priv_to_publickey(SLP, private_key)
+
+    firebase.database().ref('users/' + userId).set({
+        private_key: private_key,
+        cash_addr: cash_addr,
+        slp_addr: slp_addr,
+        email: user.email
+      });
+
+  });
 
